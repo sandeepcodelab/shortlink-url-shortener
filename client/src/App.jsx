b4ee913437b4213e8a3bb7185614071bafad4312
sandeepcodelab <sandeep.codes1@gmail.com>
2026-04-25 17:30:42 +0530

    added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "../src/components/Header/Header";
 import HomePage from "./pages/HomePage";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
     <>
       <Header />
       <HomePage />
+      <Footer />
     </>
   );
 }
