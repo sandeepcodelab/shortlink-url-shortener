74c6cc6f8899853ededc7a5b869d9f382849bf82
sandeepcodelab <sandeep.codes1@gmail.com>
2026-04-30 18:24:26 +0530

    fixed layout by outlet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import Header from "../src/components/Header/Header";
-import HomePage from "./pages/HomePage";
 import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router";
 
 export default function App() {
   return (
     <>
       <Header />
-      <HomePage />
+      <Outlet />
       <Footer />
     </>
   );
