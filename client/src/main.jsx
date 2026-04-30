a1389cd83854a7df164e78d06ada8b5d1c5a82f0
sandeepcodelab <sandeep.codes1@gmail.com>
2026-04-30 18:24:58 +0530

    added router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ToastProvider } from "./context/ToastProvider.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./pages/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [{ index: true, element: <Home /> }],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ToastProvider>
-      <App />
+      <RouterProvider router={router} />
     </ToastProvider>
   </StrictMode>,
 );
