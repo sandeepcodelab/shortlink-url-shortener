b247722386e030d20fcdf954a444381d382a1f9c
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-01 19:03:20 +0530

    added middleware in route

--- server/src/routes/url.routes.js
+++ server/src/routes/url.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createShortUrl } from "../controllers/url.controllers.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/shorten").post(createShortUrl);
+router.route("/shorten").post(authMiddleware, createShortUrl);
 
 export default router;
