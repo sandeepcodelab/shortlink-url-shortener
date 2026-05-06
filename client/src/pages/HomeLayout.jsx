92574d780ba33c6461e3c0f6bd81c134f5d2c9d3
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-06 18:47:52 +0530

    added layout pages

---
+++ client/src/pages/HomeLayout.jsx
@@ -0,0 +1,33 @@
+import { Box, Container } from "@mui/material";
+import About from "../components/Home/About";
+import Features from "../components/Home/Features";
+import Hero from "../components/Home/Hero";
+
+export default function HomeLayout() {
+  return (
+    <>
+      <Box
+        sx={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          zIndex: -1,
+          background: "radial-gradient(circle at top, #27364d, #020617 70%)",
+        }}
+      />
+      <Box
+        sx={{
+          position: "relative",
+        }}
+      >
+        <Container maxWidth="xl">
+          <Hero />
+          <Features />
+          <About />
+        </Container>
+      </Box>
+    </>
+  );
+}
