8eba3715f26a17ee8c5705a62473b9f82afd34c6
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-09 18:41:00 +0530

    added routers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,32 @@
-import Header from "../src/components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import { Outlet } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
+import HomeLayout from "./layouts/HomeLayout";
+import HomePage from "./pages/Home";
+import DashboardPage from "./pages/Dashboard";
+import DashboardLayout from "./layouts/DashboardLayout";
+import { ToastProvider } from "./context/ToastProvider";
+import Analytics from "./pages/Analytics";
+import { AuthProvider } from "./context/AuthContext";
+import { ModalProvider } from "./context/ModalContext";
 
 export default function App() {
   return (
-    <>
-      <Header />
-      <Outlet />
-      <Footer />
-    </>
+    <ModalProvider>
+      <AuthProvider>
+        <ToastProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route element={<HomeLayout />}>
+                <Route path="/" element={<HomePage />} />
+              </Route>
+
+              <Route element={<DashboardLayout />}>
+                <Route path="/dashboard" element={<DashboardPage />} />
+                <Route path="/mylinks" element={<Analytics />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </ToastProvider>
+      </AuthProvider>
+    </ModalProvider>
   );
 }
