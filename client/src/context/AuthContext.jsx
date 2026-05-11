1354f4e0c0ef7cbf78701912673f318c6166a386
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-11 16:52:03 +0530

    added functionality to get current user

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,10 +1,37 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { currentUser, refreshToken } from "../services/authService";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await currentUser();
+
+        setUser(res.data.data.user);
+      } catch (err) {
+        if (err.status === 401) {
+          try {
+            await refreshToken();
+
+            const res = await currentUser();
+            setUser(res.data.data.user);
+          } catch (error) {
+            setUser(null);
+          }
+          setUser(null);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getUser();
+  }, []);
 
   return (
     <AuthContext.Provider
