6b71e68327ab588f907a26f28d2fc3a20456d1d2
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-11 16:51:06 +0530

    added services for regresh token, get current user and logout

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -8,4 +8,16 @@ const loginUser = (formData = {}) => {
   return api.post("/auth/login", { ...formData });
 };
 
-export { registerUser, loginUser };
+const currentUser = () => {
+  return api.get("/auth/current-user");
+};
+
+const refreshToken = () => {
+  return api.post("/auth/refresh-token");
+};
+
+const logout = () => {
+  return api.post("/auth/logout");
+};
+
+export { registerUser, loginUser, currentUser, refreshToken, logout };
