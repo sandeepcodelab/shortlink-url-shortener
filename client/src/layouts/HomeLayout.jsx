3ab1471e2129d00ac4f855a2a3851c1e3498531e
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-11 16:58:06 +0530

    cleaned up home layout

--- client/src/layouts/HomeLayout.jsx
+++ client/src/layouts/HomeLayout.jsx
@@ -5,27 +5,11 @@ import Footer from "../components/Footer/Footer";
 import { Outlet } from "react-router";
 
 export default function HomeLayout() {
-  // const [open, setOpen] = useState(false);
-  // const [formType, setFormType] = useState("login");
-
   return (
     <>
-      <Header
-      // openModal={(type) => {
-      //   setFormType(type);
-      //   setOpen(true);
-      // }}
-      />
-
+      <Header />
       <Outlet />
-
-      <AuthModal
-      // open={open}
-      // handleClose={() => setOpen(false)}
-      // formType={formType}
-      // setFormType={setFormType}
-      />
-
+      <AuthModal />
       <Footer />
     </>
   );
