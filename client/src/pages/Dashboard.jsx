1dde5591d1129a252c74567da040492230dfb292
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-11 11:47:50 +0530

    added pages

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import { Box, Container } from "@mui/material";
+import Hero from "../components/Home/Hero";
+import Stats from "../components/Dashboard/Stats";
+import LinksTable from "../components/Dashboard/LinksTable";
+
+export default function DashboardPage() {
+  const [links, setLinks] = useState([]);
+
+  // 🔹 Dummy data (replace later with API)
+  useEffect(() => {
+    const dummyLinks = [
+      {
+        id: 1,
+        shortUrl: "short.ly/abc123",
+        originalUrl: "https://google.com",
+        clicks: 12,
+      },
+      {
+        id: 2,
+        shortUrl: "short.ly/dev456",
+        originalUrl: "https://github.com",
+        clicks: 5,
+      },
+      {
+        id: 3,
+        shortUrl: "short.ly/ui789",
+        originalUrl: "https://youtube.com",
+        clicks: 25,
+      },
+    ];
+
+    setLinks(dummyLinks);
+  }, []);
+
+  return (
+    <>
+      <Box
+        sx={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          zIndex: -1,
+          background: "radial-gradient(circle at top, #27364d, #020617 70%)",
+        }}
+      />
+      <Box
+        sx={{
+          position: "relative",
+        }}
+      >
+        <Container maxWidth="xl">
+          <Hero />
+          {/* <Stats links={links} /> */}
+          {/* <LinksTable links={links} setLinks={setLinks} /> */}
+        </Container>
+      </Box>
+    </>
+  );
+}
