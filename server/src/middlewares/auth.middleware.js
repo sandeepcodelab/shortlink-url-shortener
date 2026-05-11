75feb8bb2274dfcbc8977a86c398e065dc896362
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-11 16:43:02 +0530

    added functionality to verify jwt

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,4 +1,7 @@
 import { nanoid } from "nanoid";
+import jwt from "jsonwebtoken";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
 
 const authMiddleware = async (req, res, next) => {
   let guest = req.cookies?.guest;
@@ -19,4 +22,32 @@ const authMiddleware = async (req, res, next) => {
   next();
 };
 
-export { authMiddleware };
+const verifyJWT = async (req, res, next) => {
+  const token = req.cookies?.accessToken;
+
+  if (!token) {
+    throw new ApiError(401, "Unauthorized access. Please login to continue.");
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user)
+      throw new ApiError(401, "Invalid token. Please login again to continue.");
+
+    req.user = user;
+
+    next();
+  } catch (err) {
+    throw new ApiError(
+      401,
+      err?.message || "Invalid token. Please login again to continue."
+    );
+  }
+};
+
+export { authMiddleware, verifyJWT };
