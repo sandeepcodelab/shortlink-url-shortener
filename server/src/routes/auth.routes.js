32fbb5ca4ed63f777e32e5af1ac302228712cf2e
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-11 16:47:06 +0530

    added routes for auth

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
-import { login, register } from "../controllers/auth.controllers.js";
+import {
+  currentUser,
+  login,
+  logout,
+  refreshAccessToken,
+  register,
+} from "../controllers/auth.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/signup").post(register);
 router.route("/login").post(login);
+router.route("/current-user").get(verifyJWT, currentUser);
+router.route("/logout").post(verifyJWT, logout);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
