b3485c84e525d2c3d04238ad319417bacb258704
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-12 23:46:37 +0530

    fixed code structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,22 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ToastProvider } from "./context/ToastProvider.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router";
-import Home from "./pages/Home.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [{ index: true, element: <Home /> }],
-  },
-]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ToastProvider>
-      <RouterProvider router={router} />
-    </ToastProvider>
+    <App />
   </StrictMode>,
 );
