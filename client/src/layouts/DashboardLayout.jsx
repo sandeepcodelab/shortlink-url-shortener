61a92e5bb89e4dabdb392af27426c2cb2365a224
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-14 18:14:33 +0530

    added sidebar and their menus

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -1,13 +1,38 @@
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import { Outlet } from "react-router";
+import AppSidebar from "../components/Dashboard/Sidebar";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import LinkIcon from "@mui/icons-material/Link";
+import AddIcon from "@mui/icons-material/Add";
+import { useAuth } from "../context/AuthContext";
 
 export default function DashboardLayout() {
+  const { user } = useAuth();
+
+  const menuItems = [
+    {
+      title: "Dashboard",
+      path: "/dashboard",
+      menuIcon: <DashboardIcon />,
+    },
+    {
+      title: "Create Link",
+      path: "/createlinks",
+      menuIcon: <AddIcon />,
+    },
+    {
+      title: "My Links",
+      path: "/mylinks",
+      menuIcon: <LinkIcon />,
+    },
+  ];
+
   return (
     <>
-      <Header />
-      <Outlet />
-      <Footer />
+      <AppSidebar menuItems={menuItems} user={user}>
+        <Outlet />
+      </AppSidebar>
     </>
   );
 }
