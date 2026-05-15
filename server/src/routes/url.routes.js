5d5edbae0c92adef4c2f5627265dbe9287de0995
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-15 23:36:08 +0530

    added route to get all links

--- server/src/routes/url.routes.js
+++ server/src/routes/url.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { createShortUrl } from "../controllers/url.controllers.js";
-import { authMiddleware } from "../middlewares/auth.middleware.js";
+import {
+  createShortUrl,
+  getUserAllLinks,
+} from "../controllers/url.controllers.js";
+import { authMiddleware, verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/shorten").post(authMiddleware, createShortUrl);
+
+router.route("/getAllLinks/").get(verifyJWT, getUserAllLinks);
 
 export default router;
