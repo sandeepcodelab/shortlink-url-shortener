0714a0044e30951230d3cc2e2331b40ea4a71be3
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-15 23:31:04 +0530

    update middlewere

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -3,25 +3,6 @@ import jwt from "jsonwebtoken";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.models.js";
 
-const authMiddleware = async (req, res, next) => {
-  let guest = req.cookies?.guest;
-
-  const options = {
-    HttpOnly: true,
-    Secure: process.env.NODE_ENV === "production",
-    SameSite: "Lax",
-  };
-
-  if (!guest) {
-    guest = nanoid(20);
-    res.cookie("guest", guest, options);
-  }
-
-  req.guest = guest;
-
-  next();
-};
-
 const verifyJWT = async (req, res, next) => {
   const token = req.cookies?.accessToken;
 
@@ -50,4 +31,48 @@ const verifyJWT = async (req, res, next) => {
   }
 };
 
-export { authMiddleware, verifyJWT };
+const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.cookies?.accessToken;
+
+    // User auth
+    if (token) {
+      const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+      const user = await User.findById(decodedToken._id).select(
+        "-password -refreshToken"
+      );
+
+      if (!user) {
+        throw new ApiError(401, "Invalid token. Please login again.");
+      }
+
+      req.user = user;
+
+      return next();
+    }
+
+    // Guset auth
+    let guest = req.cookies?.guest;
+
+    const options = {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Lax",
+    };
+
+    if (!guest) {
+      guest = nanoid(20);
+
+      res.cookie("guest", guest, options);
+    }
+
+    req.guest = guest;
+
+    next();
+  } catch (err) {
+    next(new ApiError(401, err?.message || "Authentication failed"));
+  }
+};
+
+export { verifyJWT, authMiddleware };
