d352bd2351e58c8f1bb96ccfcb42708a68be1192
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-17 17:08:51 +0530

    added a new route

--- server/src/routes/url.routes.js
+++ server/src/routes/url.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   createShortUrl,
+  getDashboardInfo,
   getUserAllLinks,
 } from "../controllers/url.controllers.js";
 import { authMiddleware, verifyJWT } from "../middlewares/auth.middleware.js";
@@ -10,5 +11,6 @@ const router = Router();
 router.route("/shorten").post(authMiddleware, createShortUrl);
 
 router.route("/getAllLinks/").get(verifyJWT, getUserAllLinks);
+router.route("/dashboard-info/").get(verifyJWT, getDashboardInfo);
 
 export default router;
