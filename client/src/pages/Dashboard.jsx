c034d6a6f4256e88da68a80cdcf0ecd27475e9f1
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-18 22:51:06 +0530

    fixed API request

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,36 +1,73 @@
 import { useState, useEffect } from "react";
-import { Box, Container } from "@mui/material";
+import { Box, Container, Paper } from "@mui/material";
 import Stats from "../components/Dashboard/Stats";
 import LinksTable from "../components/Dashboard/LinksTable";
+import { useAuth } from "../context/AuthContext";
+import { getAllLinks, getdashboardInfo } from "../services/linkServices";
+import { useToast } from "../context/ToastProvider";
 
 export default function DashboardPage() {
-  const [links, setLinks] = useState([]);
+  const { user } = useAuth();
+  const { notify } = useToast();
+  const [linksData, setLinksData] = useState({});
 
-  // 🔹 Dummy data (replace later with API)
   useEffect(() => {
-    const dummyLinks = [
-      {
-        id: 1,
-        shortUrl: "short.ly/abc123",
-        originalUrl: "https://google.com",
-        clicks: 12,
-      },
-      {
-        id: 2,
-        shortUrl: "short.ly/dev456",
-        originalUrl: "https://github.com",
-        clicks: 5,
-      },
-      {
-        id: 3,
-        shortUrl: "short.ly/ui789",
-        originalUrl: "https://youtube.com",
-        clicks: 25,
-      },
-    ];
+    if (!user) return;
 
-    setLinks(dummyLinks);
+    const fetchData = async () => {
+      try {
+        const getlinks = await getdashboardInfo();
+
+        setLinksData(getlinks.data.data);
+      } catch (err) {
+        notify.error(err.response.data.message);
+      }
+    };
+    fetchData();
   }, []);
+
+  const columns = [
+    {
+      id: "shortCode",
+      label: "Short URL",
+      minWidth: 150,
+      format: (value) => `${import.meta.env.VITE_BASE_URL}/${value}`,
+    },
+    {
+      id: "originalUrl",
+      label: "Original URL",
+      minWidth: 100,
+      maxWidth: 200,
+    },
+    {
+      id: "clicks",
+      label: "Clicks",
+      minWidth: 100,
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "createdAt",
+      label: "Created At",
+      minWidth: 200,
+      align: "right",
+      format: (value) =>
+        new Date(value).toLocaleDateString("en-IN", {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+    },
+    {
+      id: "copy",
+      label: "Copy",
+      minWidth: 100,
+      align: "right",
+      format: (value) => value,
+    },
+  ];
 
   return (
     <>
@@ -50,9 +87,30 @@ export default function DashboardPage() {
           position: "relative",
         }}
       >
-        <Container maxWidth="xl" sx={{ mt: 8 }}>
-          <Stats links={links} />
-          <LinksTable links={links} setLinks={setLinks} sx={{ mt: 6 }} />
+        <Container
+          sx={{ display: "flex", flexDirection: "column", gap: 6, mt: 2 }}
+        >
+          <Stats
+            links={linksData.links}
+            totalLinks={linksData.totalLink}
+            totalClicks={linksData.totalClicks}
+          />
+
+          <Paper
+            elevation={0}
+            sx={{
+              overflow: "hidden",
+              bgcolor: "#19263b",
+              border: "1px solid #334155",
+              borderRadius: 3,
+            }}
+          >
+            <LinksTable
+              columns={columns}
+              rows={linksData.links}
+              pagination={false}
+            />
+          </Paper>
         </Container>
       </Box>
     </>
