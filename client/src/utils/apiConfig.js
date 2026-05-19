8f94f89b05eee442407252b928905f5040a4f386
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-19 18:12:38 +0530

    add interceptor for API request

--- client/src/utils/apiConfig.js
+++ client/src/utils/apiConfig.js
@@ -5,4 +5,34 @@ const api = axios.create({
   withCredentials: true,
 });
 
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (!error.response) {
+      return Promise.reject(error);
+    }
+
+    const originalRequest = error.config;
+
+    if (
+      error.response.status === 401 &&
+      !originalRequest._retry &&
+      !originalRequest.url.includes("/auth/refresh-token") &&
+      !originalRequest.url.includes("/auth/login")
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        await api.post("/auth/refresh-token");
+        return api(originalRequest);
+      } catch (refreshError) {
+        // Stop infinite loop
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
+
 export default api;
