edc37b9dc3c0edf1b41bbdaf8ecc44d10d0413ef
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-19 18:30:32 +0530

    fixed error and API request

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,12 +1,17 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { currentUser, refreshToken } from "../services/authService";
+import { currentUser, logout, refreshToken } from "../services/authService";
+import { useNavigate } from "react-router";
+import { useToast } from "./ToastProvider";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const { notify } = useToast();
 
+  // Get current user
   useEffect(() => {
     const getUser = async () => {
       try {
@@ -14,17 +19,7 @@ export const AuthProvider = ({ children }) => {
 
         setUser(res.data.data.user);
       } catch (err) {
-        if (err.status === 401) {
-          try {
-            await refreshToken();
-
-            const res = await currentUser();
-            setUser(res.data.data.user);
-          } catch (error) {
-            setUser(null);
-          }
-          setUser(null);
-        }
+        if (err.status === 401) setUser(null);
       } finally {
         setLoading(false);
       }
@@ -33,9 +28,20 @@ export const AuthProvider = ({ children }) => {
     getUser();
   }, []);
 
+  // LogoutUser
+  const userLogout = async () => {
+    try {
+      await logout();
+      setUser(null);
+      navigate("/");
+    } catch (err) {
+      notify.error(err.response.data.message);
+    }
+  };
+
   return (
     <AuthContext.Provider
-      value={{ user, setUser, isAuthenticated: !!user, loading }}
+      value={{ user, setUser, isAuthenticated: !!user, loading, userLogout }}
     >
       {children}
     </AuthContext.Provider>
