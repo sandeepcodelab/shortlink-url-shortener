d9286e549d4e25dc9c516a1414f24a4669779c09
sandeepcodelab <sandeep.codes1@gmail.com>
2026-05-20 19:32:30 +0530

    fixed cross origin

--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 
 // Cors config
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
   credentials: true,
 };
 
